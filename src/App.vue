21db25c62997b03c0dfabc10027f004952be45d9
David Arelano <dtarellano1@gmail.com>
2018-01-19 17:38:02 -0800

    created navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
 <div id="app">
-  <b-nav pills>
-    <b-nav-item active>Active</b-nav-item>
-    <b-nav-item>Link</b-nav-item>
-    <b-nav-item-dropdown id="nav7_ddown" text="Dropdown" right>
-      <b-dropdown-item>one</b-dropdown-item>
-      <b-dropdown-item>two</b-dropdown-item>
-      <b-dropdown-divider></b-dropdown-divider>
-      <b-dropdown-item>three</b-dropdown-item>
-    </b-nav-item-dropdown>
-  </b-nav>
+  <b-navbar toggleable="md">
+
+  <!-- <b-navbar-toggle target="nav_collapse"></b-navbar-toggle> -->
+
+  <b-collapse is-nav id="nav_collapse">
+
+    <b-navbar-nav class="ml-auto">
+      <b-nav-item-dropdown text="Projects" right>
+        <b-dropdown-item href="#">Ride Share</b-dropdown-item>
+        <b-dropdown-item href="#">Siren</b-dropdown-item>
+        <b-dropdown-item href="#">News Stand</b-dropdown-item>
+        <b-dropdown-item href="#">Image Finder</b-dropdown-item>
+      </b-nav-item-dropdown>
+    </b-navbar-nav>
+
+  </b-collapse>
+</b-navbar>
   <h1>{{ hello }}</h1>
 </div>
 </template>
