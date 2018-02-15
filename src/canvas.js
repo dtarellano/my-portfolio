a4adf24f64b646f70b268678a0ce596efc9a5c0a
David Arelano <dtarellano1@gmail.com>
2018-02-15 13:57:58 -0800

    first block succesfully rendering

--- src/canvas.js
+++ src/canvas.js
@@ -9,6 +9,13 @@ const canvas = () => {
   app.renderer.view.style.display = 'block';
   app.renderer.autoResize = true;
   app.renderer.resize(window.innerWidth, window.innerHeight);
+  let rectangle = new PIXI.Graphics();
+  rectangle.beginFill(0x66ccff);
+  rectangle.drawRect(0, 0, 64, 64);
+  rectangle.endFill();
+  rectangle.x = 170;
+  rectangle.y = 170;
+  app.stage.addChild(rectangle);
   document.body.appendChild(app.view);
 };
 
