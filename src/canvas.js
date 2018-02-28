7dd035f921f84011a71a952b513fa3167a726454
David Arelano <dtarellano1@gmail.com>
2018-02-28 13:03:22 -0800

    pixels rendering correctly

--- src/canvas.js
+++ src/canvas.js
@@ -12,12 +12,13 @@ const canvas = () => {
 
   const leftPixels = () => {
     setInterval(() => {
+      let entry = Math.random() * (window.innerWidth - 1) + 1;
       let rectangle = new PIXI.Graphics();
       rectangle.beginFill(0x21a85e);
       rectangle.drawRect(0, 0, 32, 32);
       rectangle.endFill();
       rectangle.x = 0;
-      rectangle.y = 0;
+      rectangle.y = entry;
       app.stage.addChild(rectangle);
       function setup() {
         app.ticker.add(delta => loop(delta));
@@ -26,60 +27,71 @@ const canvas = () => {
         rectangle.x += 5 + delta;
       }
       setup();
+    }, 1000);
+  };
+  leftPixels();
+
+  const rightPixels = () => {
+    setInterval(() => {
+      let entry = Math.random() * (window.innerHeight - 1) + 1;
+      let rectangle = new PIXI.Graphics();
+      rectangle.beginFill(0x21a85e);
+      rectangle.drawRect(0, 0, 32, 32);
+      rectangle.endFill();
+      rectangle.x = window.innerWidth;
+      rectangle.y = entry;
+      app.stage.addChild(rectangle);
+      function setup() {
+        app.ticker.add(delta => loop(delta));
+      }
+      function loop(delta) {
+        rectangle.x -= 5 + delta;
+      }
+      setup();
     }, 3000);
   };
+  rightPixels();
 
-  setInterval(() => {
-    let rectangle = new PIXI.Graphics();
-    rectangle.beginFill(0x21a85e);
-    rectangle.drawRect(0, 0, 32, 32);
-    rectangle.endFill();
-    rectangle.x = 0;
-    rectangle.y = window.innerHeight;
-    app.stage.addChild(rectangle);
-    function setup() {
-      app.ticker.add(delta => loop(delta));
-    }
-    function loop(delta) {
-      rectangle.y -= 5 + delta;
-    }
-    setup();
-  }, 3000);
-
-  setInterval(() => {
-    let rectangle = new PIXI.Graphics();
-    rectangle.beginFill(0x21a85e);
-    rectangle.drawRect(0, 0, 32, 32);
-    rectangle.endFill();
-    rectangle.x = 0;
-    rectangle.y = 0;
-    app.stage.addChild(rectangle);
-    function setup() {
-      app.ticker.add(delta => loop(delta));
-    }
-    function loop(delta) {
-      rectangle.y += 5 + delta;
-    }
-    setup();
-  }, 3000);
-
-  setInterval(() => {
-    let rectangle = new PIXI.Graphics();
-    rectangle.beginFill(0x21a85e);
-    rectangle.drawRect(0, 0, 32, 32);
-    rectangle.endFill();
-    rectangle.x = window.innerWidth;
-    rectangle.y = 0;
-    app.stage.addChild(rectangle);
-    function setup() {
-      app.ticker.add(delta => loop(delta));
-    }
-    function loop(delta) {
-      rectangle.x -= 5 + delta;
-    }
-    setup();
-  }, 3000);
-
+  const topPixels = () => {
+    setInterval(() => {
+      let entry = Math.random() * (window.innerWidth - 1) + 1;
+      let rectangle = new PIXI.Graphics();
+      rectangle.beginFill(0x21a85e);
+      rectangle.drawRect(0, 0, 32, 32);
+      rectangle.endFill();
+      rectangle.x = entry;
+      rectangle.y = 0;
+      app.stage.addChild(rectangle);
+      function setup() {
+        app.ticker.add(delta => loop(delta));
+      }
+      function loop(delta) {
+        rectangle.y += 5 + delta;
+      }
+      setup();
+    }, 3000);
+  };
+  topPixels();
+  const bottomPixels = () => {
+    setInterval(() => {
+      let entry = Math.random() * (window.innerHeight - 1) + 1;
+      let rectangle = new PIXI.Graphics();
+      rectangle.beginFill(0x21a85e);
+      rectangle.drawRect(0, 0, 32, 32);
+      rectangle.endFill();
+      rectangle.x = entry;
+      rectangle.y = window.innerWidth;
+      app.stage.addChild(rectangle);
+      function setup() {
+        app.ticker.add(delta => loop(delta));
+      }
+      function loop(delta) {
+        rectangle.y -= 5 + delta;
+      }
+      setup();
+    }, 3000);
+  };
+  bottomPixels();
   document.body.appendChild(app.view);
 };
 
