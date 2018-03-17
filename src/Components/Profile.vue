c47b00b315f7a916de32ea90fcf86751823d2359
David Arelano <dtarellano1@gmail.com>
2018-03-17 11:02:35 -0700

    fixed positioning

--- src/Components/Profile.vue
+++ src/Components/Profile.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="profile container">
-    <div class="row">
+    <div class="col-12">
       <h1>David Arellano</h1>
     </div>
+    <div class="col-12">
+      <div class="list-header">Software Engineer</div>
+    </div>
     <div class="row text-center">
-      <ul class="col-12">
-        <li class="list-header">Software Engineer</li>
+      <ul class="col-6">
         <li class="list-header">Frameworks</li>
         <li>React</li>
         <li>Vue.JS</li>
@@ -14,10 +16,11 @@
         <li>Serverless</li>
         <li>Boostrap</li>
       </ul>
-      <ul class="col-12">
+      <ul class="col-6">
         <li class="list-header">Dev Tools</li>
         <li>Webpack</li>
         <li>Git</li>
+        <li>Chrome Dev Tools</li>
         <li>Mocha/Chai</li>
         <li>Docker</li>
         <li>AWS Lambda</li>
@@ -29,13 +32,15 @@
         <li>MySQL</li>
         <li>Postgres</li>
       </ul>
-      <div class="link-container">
-        <i class="fab fa-linkedin">
-          <a href="https://www.linkedin.com/in/davidtarellano/"></a></i>
-        <i class="fab fa-github-square">
-          <a href="https://www.github.com/dtarellano"></a>
-        </i>
-        <h3>contact@dtarellano.com</h3>
+      <div class="col-12">
+        <div class="container">
+          <i class="fab fa-linkedin col-3">
+            <a href="https://www.linkedin.com/in/davidtarellano/"></a></i>
+          <i class="fab fa-github-square col-3">
+            <a href="https://www.github.com/dtarellano"></a>
+          </i>
+          <p class="col-6">contact@dtarellano.com</p>
+        </div>
       </div>
     </div>
     </div>
@@ -47,37 +52,40 @@
 </script>
 
 <style>
-  .profile {
-    width: 50%;
-    height: 90%;
-    position: absolute;
-    color: white;
-    background: rgba(0, 0, 0, 0.6);
-  }
-  .list-header {
-    font-size: 1.2em;
-    font-weight: bold;
-  }
-  ul {
+.profile {
+  width: 50%;
+  height: 90%;
+  position: absolute;
+  color: white;
+  background: rgba(0, 0, 0, 0.6);
+}
+.list-header {
+  font-size: 1.2em;
+  font-weight: bold;
+}
+ul {
   list-style-type: none;
-  }
-  .list {
-    float: left;
-  }
-  .link-container {
-    position: relative;
-    float: right;
-    left: 20%;
-  }
-  h1 {
-    font-size: 4em;
-  }
+}
+.list {
+  float: left;
+}
+.link-container {
+  position: relative;
+  float: right;
+  left: 20%;
+}
+h1 {
+  font-size: 4em;
+}
 
-  .fa-linkedin {
-    font-size: 50px;
-  }
+.fa-linkedin {
+  font-size: 50px;
+}
 
-  .fa-github-square {
-    font-size: 50px;
-  }
+.fa-github-square {
+  font-size: 50px;
+}
+
+p {
+}
 </style>
