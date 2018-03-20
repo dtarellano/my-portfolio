eae87bef58327e755a4e79b0adff277c837fb5c5
David Arelano <dtarellano1@gmail.com>
2018-03-20 15:45:49 -0700

    fade effect now working

--- src/canvas.js
+++ src/canvas.js
@@ -9,13 +9,20 @@ const canvas = () => {
   app.renderer.view.style.display = 'block';
   app.renderer.autoResize = true;
   app.renderer.resize(window.innerWidth, window.innerHeight);
+  // app.renderer.preserveDrawingBuffer = true;
+  // app.renderer.clearBeforeRender = false;
+  // app.renderer.transparent = true;
+  // console.log(app);
 
   const leftPixels = () => {
     setInterval(() => {
       let entry = Math.random() * (window.innerWidth - 50) + 1;
+      let container = new PIXI.Container();
       let rectangle = new PIXI.Graphics();
-      rectangle.beginFill(0x21a85e);
-      rectangle.drawRect(0, 0, 24, 24);
+      const size = 200;
+      // rectangle.blendMode = PIXI.BLEND_MODES.MULTIPLY;
+      rectangle.beginFill(0x37fc90, 0.4);
+      rectangle.drawCircle(0, 0, 10);
       rectangle.endFill();
       rectangle.x = 0;
       rectangle.y = entry;
@@ -23,7 +30,7 @@ const canvas = () => {
       function setup() {
         app.ticker.add(delta => {
           if (rectangle.x >= 1400) {
-            rectangle.clear();
+            container.removeChildren();
             return;
           }
 
@@ -32,93 +39,108 @@ const canvas = () => {
       }
       function loop(delta) {
         rectangle.x += 3 + delta;
+        let newRectangle = new PIXI.Graphics();
+        newRectangle.beginFill(0x21a85e);
+        newRectangle.drawRect(0, 0, 12, 12);
+        newRectangle.endFill();
+        newRectangle.x = rectangle.x;
+        newRectangle.y = rectangle.y;
+        container.addChild(newRectangle);
+        if (container.children.length > size) {
+          container.removeChildAt(0);
+        }
+        let len = container.children.length;
+        while (len--) {
+          container.children[len].alpha -= 0.01;
+        }
       }
       setup();
+      app.stage.addChild(container);
     }, 1000);
   };
   leftPixels();
-
-  const rightPixels = () => {
-    setInterval(() => {
-      let entry = Math.random() * (window.innerHeight - 1) + 1;
-      let rectangle = new PIXI.Graphics();
-      rectangle.beginFill(0x21a85e);
-      rectangle.drawRect(0, 0, 24, 24);
-      rectangle.endFill();
-      rectangle.x = window.innerWidth;
-      rectangle.y = entry;
-      app.stage.addChild(rectangle);
-      function setup() {
-        app.ticker.add(delta => {
-          if (rectangle.x < -10) {
-            rectangle.clear();
-            return;
-          }
-          loop(delta);
-        });
-      }
-      function loop(delta) {
-        rectangle.x -= 3 + delta;
-      }
-      setup();
-    }, 3000);
-  };
-  rightPixels();
-
-  const topPixels = () => {
-    setInterval(() => {
-      let entry = Math.random() * (window.innerWidth - 1) + 1;
-      let rectangle = new PIXI.Graphics();
-      rectangle.beginFill(0x21a85e);
-      rectangle.drawRect(0, 0, 24, 24);
-      rectangle.endFill();
-      rectangle.x = entry;
-      rectangle.y = 0;
-      app.stage.addChild(rectangle);
-      function setup() {
-        app.ticker.add(delta => {
-          if (rectangle.y > 900) {
-            rectangle.clear();
-            return;
-          }
-          loop(delta);
-        });
-      }
-      function loop(delta) {
-        rectangle.y += 3 + delta;
-      }
-      setup();
-    }, 3000);
-  };
-  topPixels();
-
-  const bottomPixels = () => {
-    setInterval(() => {
-      let entry = Math.random() * (window.innerHeight - 1) + 1;
-      let rectangle = new PIXI.Graphics();
-      console.log(rectangle);
-      rectangle.beginFill(0x21a85e);
-      rectangle.drawRect(0, 0, 24, 24);
-      rectangle.endFill();
-      rectangle.x = entry;
-      rectangle.y = window.innerWidth;
-      app.stage.addChild(rectangle);
-      function setup() {
-        app.ticker.add(delta => {
-          if (rectangle.y <= -100) {
-            rectangle.clear();
-            return;
-          }
-          loop(delta);
-        });
-      }
-      function loop(delta) {
-        rectangle.y -= 3 + delta;
-      }
-      setup();
-    }, 3000);
-  };
-  bottomPixels();
+  //
+  // const rightPixels = () => {
+  //   setInterval(() => {
+  //     let entry = Math.random() * (window.innerHeight - 1) + 1;
+  //     let rectangle = new PIXI.Graphics();
+  //     rectangle.beginFill(0x21a85e);
+  //     rectangle.drawRect(0, 0, 24, 24);
+  //     rectangle.endFill();
+  //     rectangle.x = window.innerWidth;
+  //     rectangle.y = entry;
+  //     app.stage.addChild(rectangle);
+  //     function setup() {
+  //       app.ticker.add(delta => {
+  //         if (rectangle.x < -10) {
+  //           rectangle.clear();
+  //           return;
+  //         }
+  //         loop(delta);
+  //       });
+  //     }
+  //     function loop(delta) {
+  //       rectangle.x -= 3 + delta;
+  //     }
+  //     setup();
+  //   }, 3000);
+  // };
+  // rightPixels();
+  //
+  // const topPixels = () => {
+  //   setInterval(() => {
+  //     let entry = Math.random() * (window.innerWidth - 1) + 1;
+  //     let rectangle = new PIXI.Graphics();
+  //     rectangle.beginFill(0x21a85e);
+  //     rectangle.drawRect(0, 0, 24, 24);
+  //     rectangle.endFill();
+  //     rectangle.x = entry;
+  //     rectangle.y = 0;
+  //     app.stage.addChild(rectangle);
+  //     function setup() {
+  //       app.ticker.add(delta => {
+  //         if (rectangle.y > 900) {
+  //           rectangle.clear();
+  //           return;
+  //         }
+  //         loop(delta);
+  //       });
+  //     }
+  //     function loop(delta) {
+  //       rectangle.y += 3 + delta;
+  //     }
+  //     setup();
+  //   }, 3000);
+  // };
+  // topPixels();
+  //
+  // const bottomPixels = () => {
+  //   setInterval(() => {
+  //     let entry = Math.random() * (window.innerHeight - 1) + 1;
+  //     let rectangle = new PIXI.Graphics();
+  //     console.log(rectangle);
+  //     rectangle.beginFill(0x21a85e);
+  //     rectangle.drawRect(0, 0, 24, 24);
+  //     rectangle.endFill();
+  //     rectangle.x = entry;
+  //     rectangle.y = window.innerWidth;
+  //     app.stage.addChild(rectangle);
+  //     function setup() {
+  //       app.ticker.add(delta => {
+  //         if (rectangle.y <= -100) {
+  //           rectangle.clear();
+  //           return;
+  //         }
+  //         loop(delta);
+  //       });
+  //     }
+  //     function loop(delta) {
+  //       rectangle.y -= 3 + delta;
+  //     }
+  //     setup();
+  //   }, 3000);
+  // };
+  // bottomPixels();
   document.body.appendChild(app.view);
 };
 
