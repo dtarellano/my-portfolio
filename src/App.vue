c7c0cca74ada763f7bad6e2a17fd98b36c41e618
David Arelano <dtarellano1@gmail.com>
2018-03-27 19:00:52 -0700

    fixed problem with app div having margin for no reason...

--- src/App.vue
+++ src/App.vue
@@ -35,17 +35,12 @@ body {
   background-color: #343434;
 }
 
-/*canvas {
-  position: absolute;
-}*/
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 10px;
-  margin-left: 5px;
 }
 
 h1,
